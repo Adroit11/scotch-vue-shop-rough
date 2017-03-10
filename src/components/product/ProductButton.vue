1020085696573f364a159f7b25f346c76dae5d1b
Christian Nwamba <nwambachristian@gmail.com>
2017-03-10 20:57:55 +0100

    Fixes

--- src/components/product/ProductButton.vue
+++ src/components/product/ProductButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button v-if="isAdding" class="button" @click="addToCart"><i class="fa fa-cart-plus"></i> Add to Cart</button>
-    <button v-else class="button button-danger" @click="removeFromCart(product.id)"><i class="fa fa-trash"></i> Remove from Cart</button>
+    <button v-else class="button button-danger" @click="removeFromCart(product._id)"><i class="fa fa-trash"></i> Remove from Cart</button>
   </div>
 </template>
 
